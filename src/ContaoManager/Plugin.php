8ebc9503323c10a4021c9d7a6bfcf25c2caf07b2
Marko Cupic <m.cupic@gmx.ch>
2018-09-22 12:16:25 +0200

    dev

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,28 +1,17 @@
 <?php
-/**
- * SAC Event Tool Web Plugin for Contao
- * Copyright (c) 2008-2017 Marko Cupic
- * @package sac-event-tool-bundle
- * @author Marko Cupic m.cupic@gmx.ch, 2017-2018
- * @link https://sac-kurse.kletterkader.com
- */
 
 namespace Markocupic\NavPageContainerBundle\ContaoManager;
-
 
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
-use Symfony\Component\Config\Loader\LoaderResolverInterface;
-use Symfony\Component\HttpKernel\KernelInterface;
 
 /**
  * Class Plugin
  * Plugin for the Contao Manager
- * @package Markocupic\NavPageContainerBundle\ContaoManager
+ * @package Markocupic\NavPageContainer\ContaoManager
  */
-class Plugin implements BundlePluginInterface, RoutingPluginInterface
+class Plugin implements BundlePluginInterface
 {
     /**
      * {@inheritdoc}
@@ -30,20 +19,8 @@ class Plugin implements BundlePluginInterface, RoutingPluginInterface
     public function getBundles(ParserInterface $parser)
     {
         return [
-            BundleConfig::create('Markocupic\NavPageContainerBundle\MarkocupicNavPageContainerBundle')
+            BundleConfig::create('Markocupic\NavPageContainer\MarkocupicNavPageContainer')
                 ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle'])
         ];
     }
-
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
-    {
-        return $resolver
-            ->resolve(__DIR__ . '/../Resources/config/routing.yml')
-            ->load(__DIR__ . '/../Resources/config/routing.yml');
-    }
-
 }
